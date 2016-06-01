81f9bf826b04067c4558e11863c02420f71fda96
Akshay Raj <snowcorp2@gmail.com>
2016-06-01 18:40:38 +0530

    initial commit

---
+++ app/src/main/java/akraj/snow/fcm/MainActivity.java
@@ -0,0 +1,21 @@
+package akraj.snow.fcm;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txt = (TextView)findViewById(R.id.welcome);
+
+        Intent i = getIntent();
+        txt.setText("Welcome " + i.getStringExtra("name"));
+    }
+}
