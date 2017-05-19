ecdf7e5a92dbaeda3a2b51376670f88f524f37f9
Akshay Raj <akrajilwar@users.noreply.github.com>
2017-05-19 12:23:29 +0530

    Update MyFirebaseMessagingService.java

--- app/src/main/java/akraj/snow/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/akraj/snow/fcm/MyFirebaseMessagingService.java
@@ -61,7 +61,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String tag, String messageBody, Bitmap img) {
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
